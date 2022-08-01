be855b18293ebd60689ba92391fa7a4bd0c78d92
anilvivekk <avanilvivek056@gmail.com>
2022-08-02 02:50:21 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
+import Tasks from "./Tasks";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+function App() {
+  const [newTask, setNewTask] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      taskName: newTask,
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      completed: false,
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
+  const editTask = (id)=>{
+
+  }
+
+  return (
+    <div id="main">
+      <h1>Todo App</h1>
+      <textarea id="task" placeholder="type task" onChange={handleChange} />
+      <button id="btn" onClick={addTask}>
+        Add
+      </button>
+      <div >
+        {todoList.map((task) => {
+          return (
+            <Tasks
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+			  editTask={editTask}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
-
 
 export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function (props) {
+  return (
+    <div
+      style={{
+        textDecoration: props.completed
+          ? "line-through solid rgb(0, 0, 0)"
+          : "none",
+      }}
+      className="list"
+    >
+      <h3>{props.taskName}</h3>
+      <button onClick={() => props.editTask(props.id)}>Edit</button>
+      <button onClick={() => props.deleteTask(props.id)}>Delete</button>
+      <button onClick={() => props.completeTask(props.id)}>Done</button>
+    </div>
+  );
+}
