bf15924772378e0cdb6179e463e17a403a56e9f2
anilvivekk <avanilvivek056@gmail.com>
2022-08-02 16:26:26 +0530

    updated className

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -12,8 +12,8 @@ export default function (props) {
       className="list"
     >
       <h3>{props.taskName}</h3>
-      <button onClick={() => props.editTask(props.task)}>Edit</button>
-      <button onClick={() => props.deleteTask(props.id)}>Delete</button>
+      <button className="edit" onClick={() => props.editTask(props.task)}>Edit</button>
+      <button className="delete" onClick={() => props.deleteTask(props.id)}>Delete</button>
       <button onClick={() => props.completeTask(props.id)}>Done</button>
       
     </div>
